03d7b4ed35f71df2c665e50f297c448f1de78d38
mccamyir <49794426+mccamyir@users.noreply.github.com>
2020-02-02 16:35:15 -0600

    init GameCore (#1)

---
+++ GameCore/GameCore_V2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace GameCore_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuartoGame quarto = new QuartoGame();
+            quarto.NewGame();
+
+            string command = "";
+
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    Console.Write(quarto.GameBoard[x, y]);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Playable Pieces: ");
+            Console.WriteLine(string.Join(", ", quarto.PlayablePieceList));
+            Console.WriteLine("Commands: quit, move");
+
+            while (quarto.GameOver!=true || command !="quit")
+            {
+                Console.Write("Command: ");
+                command = Console.ReadLine();
+                if(command=="move")
+                {
+                    string pieceName = "";
+                    int pieceX;
+                    int pieceY;
+                    Console.Write("Piece to move: ");
+                    pieceName = Console.ReadLine();
+                    Console.Write("X positon of the space to move to? (Counting starts at 0.): ");
+                    pieceX = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Y positon of the space to move to? (Counting starts at 0.): ");
+                    pieceY = Convert.ToInt32(Console.ReadLine());
+
+                    quarto.Move(pieceX, pieceY, pieceName);
+
+
+                    for (int x = 0; x < 4; x++)
+                    {
+                        for (int y = 0; y < 4; y++)
+                        {
+                            Console.Write(quarto.GameBoard[x, y]);
+                        }
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("Playable Pieces: ");
+                    Console.WriteLine(string.Join(", ", quarto.PlayablePieceList));
+                    Console.WriteLine("Commands: quit, move");
+                }
+            }
+
+        }
+    }
+}
---
+++ GameCore/GameCore_V2/QuartoGame.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace GameCore_V2
+{
+    public class QuartoGame
+    {
+        public bool GameOver { get; set; } = false;
+        Dictionary<string, int[]> pieceList;
+        const int MAXATTRIBUTEVARIANCE = 1;
+        public string[,] GameBoard { get; set; } = new string[4, 4];
+
+        public HashSet<string> PlayablePieceList = new HashSet<string>();
+
+
+        public QuartoGame()
+        {
+            initializePieceList();
+        }
+
+        void initializePieceList()
+        {
+            pieceList = new Dictionary<String, int[]>
+            {
+                {"[]", new int[] {2,2,2,2} },
+
+                {"A1", new int[] {0, 0, 0, 0}},
+                {"A2", new int[] {0, 0, 0, 1}},
+                {"A3", new int[] {0, 0, 1, 0}},
+                {"A4", new int[] {0, 0, 1, 1}},
+                                  
+                {"B1", new int[] {0, 1, 0, 0}},
+                {"B2", new int[] {0, 1, 0, 1}},
+                {"B3", new int[] {0, 1, 1, 0}},
+                {"B4", new int[] {0, 1, 1, 1}},
+                           
+                {"C1", new int[] {1, 0, 0, 0}},
+                {"C2", new int[] {1, 0, 0, 1}},
+                {"C3", new int[] {1, 0, 1, 0}},
+                {"C4", new int[] {1, 0, 1, 1}},
+                                  
+                {"D1", new int[] {1, 1, 0, 0}},
+                {"D2", new int[] {1, 1, 0, 1}},
+                {"D3", new int[] {1, 1, 1, 0}},
+                {"D4", new int[] {1, 1, 1, 1}}
+            };
+
+        }
+
+       /* public struct gamePiece
+        {
+            public string Name;
+            public bool Tall;
+            public bool Round;
+            public bool Pitted;
+            public bool Light;
+
+            public gamePiece(string name, bool tall, bool round, bool pitted, bool light)
+            {
+                Name = name;
+                Tall = tall;
+                Round = round;
+                Pitted = pitted;
+                Light = light;
+            }
+        }*/
+
+
+        public bool Move(int row, int col, string movedPiece)
+        {
+            if (PlayablePieceList.Contains(movedPiece) && GameBoard[row, col] == "[]")
+            {
+                GameBoard[row, col] = movedPiece;
+
+                PlayablePieceList.Remove(movedPiece);
+
+                GameOver = HasWon(row, col);
+                if (GameOver)
+                {
+                    Console.WriteLine(Environment.NewLine + "GAME OVER" + Environment.NewLine);
+                }
+                return true;
+            }
+            else return false;
+           
+        }
+
+        public bool HasWon(int currRow, int currCol)
+        {
+            bool hasWon = false;
+            /*
+            string tall = "";
+            string round = "";
+            string pitted = "";
+            string light = "";
+            */
+            HashSet<int> attr1 = new HashSet<int>();
+            HashSet<int> attr2 = new HashSet<int>();
+            HashSet<int> attr3 = new HashSet<int>();
+            HashSet<int> attr4 = new HashSet<int>();
+
+            //string pieceKey = gameBoard[currRow, currCol];
+
+            //check by row
+            for (int x = 0; x < 4; x++)
+            {
+                attr1.Add(pieceList[GameBoard[currRow, x]][0]);
+                attr2.Add(pieceList[GameBoard[currRow, x]][1]);
+                attr3.Add(pieceList[GameBoard[currRow, x]][2]);
+                attr4.Add(pieceList[GameBoard[currRow, x]][3]);
+            }
+
+            if (compareBitSet(attr1) || compareBitSet(attr2) || compareBitSet(attr3) || compareBitSet(attr4))
+            {
+                hasWon = true;
+            }
+            else
+            {
+                attr1.Clear();
+                attr2.Clear();
+                attr3.Clear();
+                attr4.Clear();
+                //check by column
+                for (int y = 0; y < 4; y++)
+                {
+                    attr1.Add(pieceList[GameBoard[y,currCol]][0]);
+                    attr2.Add(pieceList[GameBoard[y,currCol]][1]);
+                    attr3.Add(pieceList[GameBoard[y,currCol]][2]);
+                    attr4.Add(pieceList[GameBoard[y,currCol]][3]);
+                }
+                if (compareBitSet(attr1) || compareBitSet(attr2) || compareBitSet(attr3) || compareBitSet(attr4))
+                {
+                    hasWon = true;
+                }
+                //check for diagonal \
+                else if (currCol == currRow)
+                {
+                    attr1.Clear();
+                    attr2.Clear();
+                    attr3.Clear();
+                    attr4.Clear();
+                    for (int c = 0; c < 4; c++)
+                    {
+                        attr1.Add(pieceList[GameBoard[c, c]][0]);
+                        attr2.Add(pieceList[GameBoard[c, c]][1]);
+                        attr3.Add(pieceList[GameBoard[c, c]][2]);
+                        attr4.Add(pieceList[GameBoard[c, c]][3]);
+                    }
+                    if (compareBitSet(attr1) || compareBitSet(attr2) || compareBitSet(attr3) || compareBitSet(attr4))
+                    {
+                        hasWon = true;
+                    }
+                }
+                //Check for diagonal /
+                else if ((currCol + currRow) == 3)
+                {
+                    attr1.Clear();
+                    attr2.Clear();
+                    attr3.Clear();
+                    attr4.Clear();
+                    for (int c = 0; c < 4; c++)
+                    {
+                        attr1.Add(pieceList[GameBoard[c, (3 - c)]][0]);
+                        attr2.Add(pieceList[GameBoard[c, (3 - c)]][1]);
+                        attr3.Add(pieceList[GameBoard[c, (3 - c)]][2]);
+                        attr4.Add(pieceList[GameBoard[c, (3 - c)]][3]);
+                    }
+                    if (compareBitSet(attr1) || compareBitSet(attr2) || compareBitSet(attr3) || compareBitSet(attr4))
+                    {
+                        hasWon = true;
+                    }
+                }
+            }
+
+            return hasWon;
+        }
+
+        bool compareBitSet(HashSet<int> attrSet)
+        {
+            return (attrSet.Count == MAXATTRIBUTEVARIANCE);
+        }
+
+        public void NewGame()
+        {
+            GameBoard = new string[4, 4];
+            for(int x=0; x<4; x++)
+            {
+                for(int y=0; y<4; y++)
+                {
+                    GameBoard[x, y] = "[]";
+                }
+            }
+
+            PlayablePieceList = new HashSet<string> { "A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4" };
+        }
+    }
+}
