d847b0146411856f679d746e67c704744147834c
mgeorge1 <38506949+mgeorge1@users.noreply.github.com>
2020-02-18 20:50:14 -0600

    add initial HUD with basic functionality

---
+++ QuartonQuest/Assets/Scripts/clickabilityTester.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class clickabilityTester : MonoBehaviour
+{
+
+
+    public Text logText;
+    public ScrollView scroller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logText.text = "";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right clicked the board");
+            logText.text += "Right clicked the board\n";
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left clicked the board");
+            logText.text += "Left clicked the board\n";
+        }
+        //https://answers.unity.com/questions/12564/clickable-world-object.html
+    }
+}
