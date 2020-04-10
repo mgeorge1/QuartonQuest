7b1baa18658af5a399f31c9ff686924e382a536a
Jeremy Campbell <32200132+jeremyc61298@users.noreply.github.com>
2020-04-09 22:38:45 -0500

    Add skippable splash screen. (#80)

---
+++ QuartonQuest/Assets/Scripts/UI/SplashSceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class SplashSceneController : MonoBehaviour
+{
+    // Source: https://mirimad.com/unity-play-video-on-canvas/
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    // Use this for initialization
+    void Start()
+    {
+        videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+        StartCoroutine(PlayVideo());
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    {
+        GUIController.Instance.LoadSceneWithTransition(GUIController.SceneNames.MainMenu);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GUIController.Instance.LoadSceneWithTransition(GUIController.SceneNames.MainMenu);
+        }
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+            yield return null;
+        rawImage.texture = videoPlayer.texture;
+        rawImage.color = Color.white;
+        videoPlayer.Play();
+    }
+
+}
